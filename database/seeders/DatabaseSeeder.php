a1902165caad12130b9e322394ae5339c3addde1
manhtqb <nguyenthemanh.tlu@gmail.com>
2022-03-11 16:40:43 +0700

    init

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        \App\Models\User::truncate();
+        \App\Models\User::create([
+          'email' => 'test@gmail.com',
+          'password' => bcrypt('123456'),
+          'name' => 'ABC'
+        ]);
+    }
+}
